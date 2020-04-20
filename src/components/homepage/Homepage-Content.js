44e3e20cec22ade74fc33d9ada09a382ebe67293
naz_liton <nazrulliton@icloud.com>
2020-04-20 11:57:07 +0100

    inventory table created

--- src/components/homepage/Homepage-Content.js
+++ src/components/homepage/Homepage-Content.js
@@ -4,35 +4,7 @@ import { Button } from 'react-bootstrap';
 
 function HomepageContent(){
     return(
-//         <Table striped bordered hover>
-//   <thead>
-//     <tr>
-//       <th>#</th>
-//       <th>First Name</th>
-//       <th>Last Name</th>
-//       <th>Username</th>
-//     </tr>
-//   </thead>
-//   <tbody>
-//     <tr>
-//       <td>1</td>
-//       <td>Mark</td>
-//       <td>Otto</td>
-//       <td>@mdo</td>
-//     </tr>
-//     <tr>
-//       <td>2</td>
-//       <td>Jacob</td>
-//       <td>Thornton</td>
-//       <td>@fat</td>
-//     </tr>
-//     <tr>
-//       <td>3</td>
-//       <td colSpan="2">Larry the Bird</td>
-//       <td>@twitter</td>
-//     </tr>
-//   </tbody>
-// </Table>
+
 <>
 <div className="mb-2">
   <Button variant="primary" size="lg">

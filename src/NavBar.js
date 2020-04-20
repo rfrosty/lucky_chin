281b7e24c79bc4157a6e988333a2d2daa33dad85
Ryan <ryanfrost2015@gmail.com>
2020-04-20 11:39:42 +0100

    MainContainer renders NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const NavBar = (props) => {
+  return (
+    <header>
+      <img src="/logos/lucky_chen.png" alt="Logo"/>
+      <ul>
+        <li>
+          <a href="www.google.com">Inventory</a>
+        </li>
+        <li>
+          <a href="www.google.com">Suppliers</a>
+        </li>
+      </ul>
+
+
+      {/*
+      EXTENSION
+      <ul>
+        <li>Perishable</li>
+        <li>Fresh</li>
+        <li>Suppliers</li>
+        <li>About to expire (flashes red x amount of days before?)</li>
+      </ul>
+
+      PIRATES
+
+      <ul>
+        <li className="navLink"> //don't understand why we haven't used link tag. Guess links to routes work.
+          <a href="/pirates">Pirates</a>
+        </li>
+        <li className="navLink">
+        <a href="/pirates/new">Create Pirate</a>
+        </li>
+        <li className="navLink">
+          <a href="/ships">Ships</a>
+        </li>
+        <li className="navLink">
+          <a href="/ships/new">Create Ship</a>
+        </li>
+        <li className="navLink">
+          <a href="/ships/dew">Create Dew</a>
+        </li>
+
+      </ul>
+
+      */}
+
+    </header>
+  )
+}
+
+export default NavBar;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBar from '../NavBar.js'
 
 const MainContainer = () => {
   return (
     <>
-      <p>I am the MainContainer.</p>
+      <NavBar/>
     </>
   )
 }

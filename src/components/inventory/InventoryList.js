025b85eb10432474ddb20f662509951dbe915dd6
Ryan <ryanfrost2015@gmail.com>
2020-04-20 13:42:59 +0100

    InventoryContainer renders InventoryList

---
+++ src/components/inventory/InventoryList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Item from './item.js' //test if it works without dot.
+
+const InventoryList = (props) => {
+
+  // const item = [5];
+
+  return (
+    <div>I am an InventoryList.</div>
+  )
+
+}
+
+export default InventoryList;
--- src/containers/inventory/InventoryContainer.js
+++ src/containers/inventory/InventoryContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-// import InventoryList from '../../components/inventory/InventoryList.js';
+import InventoryList from '../../components/inventory/InventoryList.js';
 // import Item from '../../components/suppliers/SupplierList.js';
 
 class InventoryContainer extends Component {
@@ -8,13 +8,25 @@ class InventoryContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-      inventory: []
+      inventory: [
+        {"quantity": 5, "supplierID": 1, "name": "carrots", "date of purchase": "4/4/20", "expiry date": "4/5/20"},
+        {"quantity": 7, "supplierID": 1, "name": "wraps", "date of purchase": "4/4/20", "expiry date": "4/5/20"},
+        {"quantity": 2, "supplierID": 1, "name": "rice", "date of purchase": "4/4/20", "expiry date": "4/5/20"}
+      ]
     }
   }
 
   render(){
     return(
-      <p>I am the InventoryContainer.</p>
+      <Router>
+      <>
+      <Switch>
+        <Route render = {(props) => {
+          return <InventoryList ships={this.state.inventory}/>
+        }}/>
+      </Switch>
+      </>
+      </Router>
     )
   }
 

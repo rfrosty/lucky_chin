dc530af633e1745a5eef28daee100352679f185c
Ryan <ryanfrost2015@gmail.com>
2020-04-22 12:11:09 +0100

    SuppliersList renders Supplier

---
+++ src/components/suppliers/Supplier.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Supplier = (props) => {
+
+
+  return (
+    <>
+    <p>Supplier.</p>
+    </>
+  )
+
+
+}
+
+export default Supplier;
--- src/components/suppliers/SupplierList.js
+++ src/components/suppliers/SupplierList.js
@@ -1,24 +1,23 @@
 import React from 'react';
+import Supplier from './Supplier';
 // import Item from './Item.js' //test if it works without dot.
 
 const SupplierList = (props) => {
 
-  // const supplierList = props.suppliers.map((supplier => {
-  //   return (
-  //   <li key={supplier.id} className="list-item" >
-  //     <Item info={supplier}/>
-  //   </li>
-  //   )
-  // }))
+  const supplierList = props.suppliers.map((supplier => {
+    return (
+    <li key={supplier.id} className="list-item" >
+      <Supplier info={supplier}/>
+    </li>
+    )
+  }))
 
   return (
     <>
     <button><a href="/supplier/new">Add supplier</a></button>
-    {/*
     <ul>
-      {itemList}
+      {supplierList}
     </ul>
-    */}
     </>
   )
 
